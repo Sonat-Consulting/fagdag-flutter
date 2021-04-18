844517bae14e45bf2832a98331f7e7e5a24b172a
Arne Molland <arne@molland.sh>
2021-04-18 15:39:29 +0200

    Add implict animation examples

--- animasjoner/example/lib/pages/implicit/implicit_animations.dart
+++ animasjoner/example/lib/pages/implicit/implicit_animations.dart
@@ -1,3 +1,4 @@
+import 'package:animations_example/components/animated_list_tile.dart';
 import 'package:flutter/material.dart';
 
 class ImplicitAnimationsPage extends StatelessWidget {
@@ -6,6 +7,30 @@ class ImplicitAnimationsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Implisitte animasjoner'),
+      ),
+      body: ListView(
+        children: [
+          AnimatedListTile(
+            title: Text('Synlighet'),
+            subtitle: Text('AnimatedOpacity'),
+            onTap: () => Navigator.of(context).pushNamed('/implicit/opacity'),
+          ),
+          AnimatedListTile(
+            title: Text('Containere'),
+            subtitle: Text('AnimatedContainer'),
+            onTap: () => Navigator.of(context).pushNamed('/implicit/container'),
+          ),
+          AnimatedListTile(
+            title: Text('Bytting av widgets'),
+            subtitle: Text('AnimatedSwitcher'),
+            onTap: () => Navigator.of(context).pushNamed('/implicit/switcher'),
+          ),
+          AnimatedListTile(
+            title: Text('Hero-animasjoner'),
+            subtitle: Text('Hero'),
+            onTap: () => Navigator.of(context).pushNamed('/implicit/hero'),
+          ),
+        ],
       ),
     );
   }
