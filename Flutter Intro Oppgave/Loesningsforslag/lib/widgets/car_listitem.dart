79e30b5903f5abff70b3c76abb0c6f0cc2d3d87b
Thomas Ellingsen <thomas.ellingsen@sonat.no>
2021-04-29 22:13:55 +0200

    add start for tasks

---
+++ Flutter Intro Oppgave/Loesningsforslag/lib/widgets/car_listitem.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fagdag/data/cars_data.dart';
+import 'package:flutter_fagdag/screens/car_detail.dart';
+
+class CarListItem extends StatelessWidget {
+  final Car car;
+
+  const CarListItem({required this.car});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Stack(
+        children: <Widget>[
+          Hero(
+            tag: 'carimage${car.model}',
+            child: Image.network(
+              car.photoUrl.toString(),
+              fit: BoxFit.fill,
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            left: 10,
+            child: Text(
+              car.brand,
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white70),
+            ),
+          ),
+          Positioned(
+            top: 70,
+            left: 20,
+            child: Text(
+              car.model,
+              style: TextStyle(
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white54),
+            ),
+          ),
+        ],
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 5,
+      margin: EdgeInsets.all(10),
+    );
+  }
+}
---
+++ Flutter Intro Oppgave/OPPGAVER.md
@@ -0,0 +1,23 @@
+# Flutter Intro -oppgaver
+
+
+
+1. Stjerne-rating av bil fungerer ikke når man trykker på en stjerne under en av bilene. Finn ut hva som er galt og fiks det!
+
+   
+
+2. I "databasen" vår som ligger i filen cars.data oppstår det en feil fra tid til annen når vi prøver å laste inn listen over bilene. Faktisk ca hver 10. gang (se filen lib/data/cars_data.dart). Håndter feilen i koden og vis et ikon som passer sammen med beskrivende tekst istedenfor listen over biler.
+
+   
+
+
+
+
+
+
+
+
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
